4bef73f0cd000d617f131578cc599bb42c6cc167
Satishrocky007 <perlasatishkumar07@gmail.com>
2020-07-28 13:51:40 +0530

    adding changes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  @Input() recipe: Recipe;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
+
+
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,16 +8,24 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes :Recipe[] =[
-    new Recipe('ChickenBiryani','Non-Veg','https://cdn.pixabay.com/photo/2016/01/15/10/56/biryani-1141444_960_720.jpg'),
-    
-    new Recipe('Fish','Non-Veg','https://images.immediate.co.uk/production/volatile/sites/4/2009/07/GettyImages-931270318-43ab672.jpg?webp=true&quality=90&resize=940%2C399')
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('ChickenBiryani', 'Non-Veg', 'https://cdn.pixabay.com/photo/2016/01/15/10/56/biryani-1141444_960_720.jpg'),
+
+    new Recipe('Fish', 'Non-Veg', 'https://images.immediate.co.uk/production/volatile/sites/4/2009/07/GettyImages-931270318-43ab672.jpg?webp=true&quality=90&resize=940%2C399')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
     console.log("loading recipe list component");
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
+
   }
 
 }
