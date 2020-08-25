a38692b37fb44a92a0aa981e071290474fe2c8e6
Satishrocky007 <perlasatishkumar07@gmail.com>
2020-08-25 13:52:24 +0530

    Commenting ng destroy

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+// import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit , OnDestroy{
+export class RecipeListComponent implements OnInit{
 
-  subscription :Subscription;
+  // subscription :Subscription;
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService,
@@ -30,7 +30,7 @@ export class RecipeListComponent implements OnInit , OnDestroy{
     this.router.navigate(['new'], {relativeTo :this.route});
   }
 
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
-  }
+  // ngOnDestroy(){
+  //   this.subscription.unsubscribe();
+  // }
 }
