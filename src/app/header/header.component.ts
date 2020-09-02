fcc887b77867c540b4a4467de0619fa6edaa81f0
Satishrocky007 <perlasatishkumar07@gmail.com>
2020-09-02 13:08:28 +0530

    Fetching the data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,33 @@
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService){}
-
+export class HeaderComponent implements OnInit, OnDestroy {
+    private userSub :Subscription;
+    isAuthenticated = false;
+    constructor(private dataStorageService: DataStorageService, private authService:AuthService){}
+    
+    ngOnInit(){
+      this.userSub=  this.authService.user.subscribe(user => {
+        this.isAuthenticated =!!user;
+        console.log(!user);
+        console.log(!!user);
+      });
+    }
     onSaveData(){
         this.dataStorageService.storeRecipe();
     }
 
     onFetchData(){
         this.dataStorageService.fetchRecipe().subscribe();
+    }
+
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
     }
 }
